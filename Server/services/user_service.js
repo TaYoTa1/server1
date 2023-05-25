8fbf3657431c2170a6c809b2f797605c786bf890
TaYoTa1 <gr6371695@gmai.com>
2023-05-25 14:16:07 +0300

    новый файл:    .gitignore 	новый файл:    Server/Postman commands.txt 	новый файл:    Server/contollers/pc_controller.js 	новый файл:    Server/contollers/user_controller.js 	новый файл:    Server/index.js 	новый файл:    Server/package-lock.json 	новый файл:    Server/package.json 	новый файл:    Server/prisma/migrations/20230523082026_init/migration.sql 	новый файл:    Server/prisma/migrations/migration_lock.toml 	новый файл:    Server/prisma/schema.prisma 	новый файл:    Server/router/pc_router.js 	новый файл:    Server/router/user_router.js 	новый файл:    Server/services/pc_service.js 	новый файл:    Server/services/user_service.js 	новый файл:    "Server/\320\237\320\276\320\273\320\265\320\267\320\275\321\213\320\265 \321\201\321\201\321\213\320\273\320\272\320\270" 	новый файл:    "Server/\320\240\320\260\320\267\320\273\320\270\321\207\320\275\321\213\320\265 \320\267\320\260\320\274\320\265\321\202\320\272\320\270" 	новый файл:    package-lock.json 	новый файл:    package.json

---
+++ Server/services/user_service.js
@@ -0,0 +1,75 @@
+const { PrismaClient } = require('@prisma/client')
+
+const prisma_client = new PrismaClient()
+
+class UserService {
+    
+    async GetAllUsers() {
+        return prisma_client.user.findMany({});
+    }
+
+    async AddUser(req) {
+        const { firstName, lastName, email, numberPhone, position, jobPlace } = req.body
+
+        try {
+            var bHasEmail = await prisma_client.user.count(
+                { "where": { "email": email } },
+                
+            ) > 0 ? true : false
+            
+            var bHasPhone= await prisma_client.user.count(
+                { "where": { "numberPhone": numberPhone } },
+                
+            ) > 0 ? true : false
+            if (bHasPhone) return { "ERROR" : "PHONE EXISTS" }
+            if (bHasEmail) return { "ERROR" : "EMAIL EXISTS" }
+
+            const user = await prisma_client.user.create({
+                data: {
+                    firstName,
+                    lastName,
+                    email,
+                    numberPhone,
+                    position,
+                    jobPlace,
+                    aboutPC: {}
+                }
+            })
+            return user
+        } catch (e) {
+            return e
+        }
+
+    }
+    async DeleteUser(req) {
+        {
+            const { id } = req.body;
+            
+            await prisma_client.user.deleteMany({"where" : { "id": id}})
+            return { "error" : "SUCCESS" }
+        }
+    }
+    async UpdateUser(req)
+        {
+            const { id, key, val } = req.body;
+            if (!key)
+                return { "error": "KEY NOT PRESENT" }
+    
+            if (key == "id")
+                return { "error": "You cannot change id" }
+    
+            if (key == "email" || key == "numberPhone")
+            {
+                var bHas = await prisma_client.user.count({"where": {"id": id}, "data": {[key]: val}}) 
+                    > 0 ? true : false
+                if (bHas)
+                {
+                    return {"error" : "ERROR - DUPLICATE"}
+                }
+            }
+    
+            var usr = await prisma_client.user.update({"where": {"id": id}, "data": {[key]: val}})
+            return usr
+        }
+    }
+module.exports = new UserService;
